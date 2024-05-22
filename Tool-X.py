9d43679093fdba287202e16f93b4aeb11c1c74b5
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-05-22 11:58:29 +0600

    📝 Fixed the import statement to from modules.menu import main.

--- Tool-X.py
+++ Tool-X.py
@@ -1,61 +1,60 @@
-/*
- * Tool-X
- *
- * Tool-X is dual-licensed under the terms of the MIT License and the
- * GNU General Public License v3.0 (GPL-3.0). You may choose either
- * license to govern your use of this project.
- *
- * MIT License:
- * Permission is hereby granted, free of charge, to any person obtaining a 
+"""
+Tool-X
+
+Tool-X is dual-licensed under the terms of the MIT License and the
+GNU General Public License v3.0 (GPL-3.0). You may choose either
+license to govern your use of this project.
+
+MIT License:
+Permission is hereby granted, free of charge, to any person obtaining a 
 copy
- * of this software and associated documentation files (the "Software"), 
+of this software and associated documentation files (the "Software"), 
 to deal
- * in the Software without restriction, including without limitation the 
+in the Software without restriction, including without limitation the 
 rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or 
 sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included 
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included 
 in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
 OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
 MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
 THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
 FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 IN THE
- * SOFTWARE.
- *
- * GPL-3.0 License:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
+SOFTWARE.
+
+GPL-3.0 License:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import os
-import sys
-from modules.menu import *
+from modules.menu import main
 
-if __name__=="__main__":
-  try:
-    main.menu()
-  except KeyboardInterrupt:
-    os.system("clear")
-    logo.exit()
+if __name__ == "__main__":
+    try:
+        main.menu()
+    except KeyboardInterrupt:
+        os.system("clear")
+        main.exit()
