342645a89b935644dc8fc07ab40c003b15b98cb1
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-05-22 13:38:33 +0600

    Update update_versions.py

--- core/update_versions.py
+++ core/update_versions.py
@@ -10,6 +10,8 @@ with open(data_file_path, 'r') as file:
 
 # Function to get the latest version from GitHub
 def get_latest_version(repo_url):
+    if not repo_url:
+        return None
     api_url = repo_url.replace('github.com', 'api.github.com/repos').rstrip('.git') + '/releases/latest'
     response = requests.get(api_url)
     if response.status_code == 200:
@@ -19,7 +21,7 @@ def get_latest_version(repo_url):
 # Update the versions
 updated = False
 for tool in data.values():
-    latest_version = get_latest_version(tool['url'])
+    latest_version = get_latest_version(tool.get('url'))
     if latest_version and latest_version != tool['version']:
         tool['version'] = latest_version
         updated = True
